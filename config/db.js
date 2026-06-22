30a676a0ff74cbb7c718a881c3402fda6be608f9
Phan Nhật Nam <nhatnam.090504@gmail.com>
2026-06-22 15:07:25 +0700

    m

--- config/db.js
+++ config/db.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
+    console.error("MongoDB connection failed:");
+    console.error(error);
+
     process.exit(1);
   }
 };
